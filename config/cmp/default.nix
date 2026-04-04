33254a56b8d82c183d9ed40dd08f7736f22616df
lunabirkeland <lunabirkeland@gmail.com>
2026-04-04 15:49:20 +0100

    completion upgrades

--- config/cmp/default.nix
+++ config/cmp/default.nix
@@ -1,10 +1,49 @@
 {
   plugins = {
     nvim-autopairs.enable = true;
+    luasnip.enable = true;
     blink-cmp = {
       enable = true;
       settings = {
-        keymap.preset = "super-tab";
+        cmdline.enabled = false;
+
+        snippets.preset = "luasnip";
+
+        completion = {
+          list.selection.preselect = false;
+          documentation = {
+            auto_show = true;
+            auto_show_delay_ms = 0;
+          };
+        };
+
+        keymap = {
+          "<Tab>" = [
+            "select_next"
+            "fallback"
+          ];
+          "<S-Tab>" = [
+            "select_prev"
+            "fallback"
+          ];
+          "<CR>" = [
+            "select_and_accept"
+            "fallback"
+          ];
+        };
+
+        sources = {
+          default = [
+            "lsp"
+            "path"
+            "snippets"
+            "buffer"
+          ];
+          providers = {
+            # Words from other open buffers can also be suggested.
+            buffer.opts.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+          };
+        };
       };
     };
   };
